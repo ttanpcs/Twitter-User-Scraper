84a623710324c9126937f22aca7c0f4f919b31d7
goldenxuett <ttanpcs@gmail.com>
2022-01-22 00:00:43 -0600

    Fix main() script documentation and add os support

--- twitter-user-scraper/twitter_user_scraper_tool.py
+++ twitter-user-scraper/twitter_user_scraper_tool.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
+"""Twitter User Scraper Tool
+
+This script allows the user to find various statistics for the users
+specified in the command line arguments.
+
+This tool either accepts all non flagged parameters as string usernames
+or accepts an input file location with the flag --input.
+The --output flag allows the user to change the output location from
+'user_information.csv'
+"""
 
 import argparse
+import os
+
+bearer_token = os.environ.get("BEARER_TOKEN")
 
 def main(args):
-    """Scrape twitter for information on specified arguments"""
-    return 0
+    """Scrape twitter for information on specified arguments
+    
+    If either 'names' or '--input' are not passed, the program will stop. 
+    The program prioritizes 'names' before 'input'
+
+    Parameters
+    ----------
+    args : argument namespace
+        All command line arguments as parsed by argparse
+    """
+
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Functional Twitter user information scraper.")
