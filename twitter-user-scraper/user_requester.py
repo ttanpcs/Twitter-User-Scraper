870036beef8352a68bf61111b667dca66d180418
goldenxuett <ttanpcs@gmail.com>
2022-01-22 22:03:52 -0600

    Implement UserRequester calculate_values and create_url

--- twitter-user-scraper/user_requester.py
+++ twitter-user-scraper/user_requester.py
@@ -1,4 +1,7 @@
+import constants
 import standard_requester as sr
+import numpy as np
+import user_data as ud
 
 class UserRequester(sr.StandardRequester):
     """A requester class to calculate Twitter user PII statistics
@@ -49,16 +52,35 @@ class UserRequester(sr.StandardRequester):
             url string for request type
         """
 
+        current_users = "usernames=" + ",".join(self.users_info_set[index])
+        user_fields = "user.fields=" + ",".join(constants.USER_FIELDS)
+        url = "https://api.twitter.com/2/users/by?{}&{}".format(current_users, user_fields)
+
+        return url
+
     def calculate_values(self):
         """Calculates the requested values and returns a UserData object.
 
         Returns
         -------
-        UserData
-            UserData object with requested statistics
-        """
+        list[UserData]
+            list of UserData object with requested statistics
+        """            
+        raw_user_data = []
+        for index in range(len(self.users_info_set)):
+            raw_user_data = np.concatenate((self.connect_to_endpoint(index).get("data"), raw_user_data))
+        user_data = []
+        for user in raw_user_data:
+            processed_user = {}
+            for key in constants.USER_FIELDS:
+                if (constants.USER_SUB_FIELDS.get(key) is None):
+                    processed_user[key] = user.get(key)
+                else:
+                    for subkey in constants.USER_SUB_FIELDS.get(key):
+                        processed_user[subkey] = user.get(key).get(subkey)
+            user_data.append(ud.UserData(processed_user))
 
-        return {}
+        return user_data
 
     def get_data_name(self):
         """Returns an identifier for the requester type.
