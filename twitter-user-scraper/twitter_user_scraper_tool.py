a081981a127ca73f6ac71a9057e597792d25e2f2
goldenxuett <ttanpcs@gmail.com>
2022-01-26 15:00:06 -0600

    Implement twitter_user_scraper exe

--- twitter-user-scraper/twitter_user_scraper_tool.py
+++ twitter-user-scraper/twitter_user_scraper_tool.py
@@ -12,6 +12,8 @@ The --output flag allows the user to change the output location from
 
 import argparse
 import os
+import twitter_user_scraper as tus
+import user_data_handler as udh
 
 bearer_token = os.environ.get("BEARER_TOKEN")
 
@@ -26,15 +28,30 @@ def main(args):
     args : argument namespace
         All command line arguments as parsed by argparse
     """
+    username_list = []
+    if (len(args.names) == 0 and (args.input is None)):
+        print("Please input either a list of usernames or an input file path\n")
+        return 0
 
-    pass
+    if (len(args.names) != 0):
+        username_list = args.names
+    elif (args.input is not None):
+        with open(args.input) as infile:
+            username_list = infile.readlines()
+    scraper = tus.TwitterUserScraper(bearer_token, username_list, args.followers, args.timeline)
+    user_data_list = scraper.parse()
+    handler = udh.UserDataHandler()
+    handler.write_users_to_file(user_data_list, args.output)
+
+    return 0
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Functional Twitter user information scraper.")
     parser.add_argument("names", nargs = "*", help = "Twitter usernames to scrape information from")
-    parser.add_argument("--input", "--i", help = "input file path")
-    parser.add_argument( "--output", "--o", help = "output file path (default: user_information.json)", default = "user_information.json")
-    
-    args = parser.parse_args()
+    parser.add_argument("--input", "-i", help = "input file path")
+    parser.add_argument( "--output", "-o", help = "output file path (default: user_information.json)", default = "user_information.json")
+    parser.add_argument("--followers", "-f", help = "Adds followers statistics to dataset", action = "store_true")
+    parser.add_argument("--timeline", "-t", help = "Adds timeline statistics to dataset", action = "store_true")
 
+    args = parser.parse_args()
     main(args)
